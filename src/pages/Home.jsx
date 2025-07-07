8ded5115598f64a327961bda987164f00a79281f
DaanBaard <daanvanveen@gmail.com>
2025-07-07 22:31:01 +0200

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,16 @@ const tags = ["eten", "drinken", "cultuur", "vaenw"];
 
 function Home() {
   return (
-    <div className="container">
-      <img src={alkmaarImage} alt="Alkmaar" />
+    <div className="home">
+      <img src={alkmaarImage} alt="Alkmaar" className="hero-image" />
       <h1>Een alternatieve gids door Alkmaar</h1>
-      <div style={{ textAlign: "center" }}>
+      <p className="intro-text">
+        Ontdek unieke plekken om te eten, drinken, beleven en verdwalen in Alkmaar.
+      </p>
+      <div className="tag-buttons">
         {tags.map((tag) => (
           <Link to={`/category/${tag}`} key={tag}>
-            <button>{tag}</button>
+            <button className="tag-btn">{tag}</button>
           </Link>
         ))}
       </div>
